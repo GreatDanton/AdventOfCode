f911d2823d253f54b6a5e3af37625ac9cb133acb
GreatDanton <pribosek.jan@gmail.com>
2018-12-12 23:56:44 +0100

    Solutions were in the wrong folders, renamed folders

--- 2018/10/main.go
+++ 2018/10/main.go
@@ -1,102 +1,233 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
+	"regexp"
+	"sort"
 	"strconv"
 )
 
+type star struct {
+	x  int
+	y  int
+	vx int
+	vy int
+}
+
+/* 10.day: https://adventofcode.com/2018/day/10 */
 func main() {
-	const gridID = 6303
-	fmt.Println(coordinatePowerLever(3, 5, 8))
-
-	grid := createGrid(gridID, 300)
-	powerLevel, x, y := getMaxPowerLevelSquare(grid, 3)
-	fmt.Printf("Power level: %d, x: %d, y: %d\n", powerLevel, x, y)
-
-	maxLevel := 0
-	maxX := 0
-	maxY := 0
-	maxSquareSize := 0
-	// this is brute force approach it could be done better
-	for squareSize := 1; squareSize < 301; squareSize++ {
-		level, x, y := getMaxPowerLevelSquare(grid, squareSize)
-		if level > maxLevel {
-			maxLevel = level
-			maxX = x
-			maxY = y
-			maxSquareSize = squareSize
-			fmt.Println(x, y, maxSquareSize)
-		}
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Printf("Max level of all squares: x,y,squareSize %d,%d,%d\n", maxX, maxY, maxSquareSize)
+	scanner := bufio.NewScanner(file)
+	lines := []string{}
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	// testing input (prints HI)
+	/* 	lines = []string{
+		"position=< 9,  1> velocity=< 0,  2>",
+		"position=< 7,  0> velocity=<-1,  0>",
+		"position=< 3, -2> velocity=<-1,  1>",
+		"position=< 6, 10> velocity=<-2, -1>",
+		"position=< 2, -4> velocity=< 2,  2>",
+		"position=<-6, 10> velocity=< 2, -2>",
+		"position=< 1,  8> velocity=< 1, -1>",
+		"position=< 1,  7> velocity=< 1,  0>",
+		"position=<-3, 11> velocity=< 1, -2>",
+		"position=< 7,  6> velocity=<-1, -1>",
+		"position=<-2,  3> velocity=< 1,  0>",
+		"position=<-4,  3> velocity=< 2,  0>",
+		"position=<10, -3> velocity=<-1,  1>",
+		"position=< 5, 11> velocity=< 1, -2>",
+		"position=< 4,  7> velocity=< 0, -1>",
+		"position=< 8, -2> velocity=< 0,  1>",
+		"position=<15,  0> velocity=<-2,  0>",
+		"position=< 1,  6> velocity=< 1,  0>",
+		"position=< 8,  9> velocity=< 0, -1>",
+		"position=< 3,  3> velocity=<-1,  1>",
+		"position=< 0,  5> velocity=< 0, -1>",
+		"position=<-2,  2> velocity=< 2,  0>",
+		"position=< 5, -2> velocity=< 1,  2>",
+		"position=< 1,  4> velocity=< 2,  1>",
+		"position=<-2,  7> velocity=< 2, -2>",
+		"position=< 3,  6> velocity=<-1, -1>",
+		"position=< 5,  0> velocity=< 1,  0>",
+		"position=<-6,  0> velocity=< 2,  0>",
+		"position=< 5,  9> velocity=< 1, -2>",
+		"position=<14,  7> velocity=<-2,  0>",
+		"position=<-3,  6> velocity=< 2, -1>",
+	} */
+
+	stars := parseInput(lines)
+	displayStars(stars)
 }
 
-func createGrid(gridID int, gridSize int) [][]int {
-	grid := [][]int{}
-	for y := 0; y < gridSize; y++ {
-		xRow := make([]int, gridSize)
-		for x := 0; x < gridSize; x++ {
-			xRow[x] = coordinatePowerLever(x+1, y+1, gridID)
+var maxWidth = 100
+var maxHeight = 50
+var maxIteration = 100000
+var minMapSize = 100000000
+
+// iteration where coordinates produced the smallest bounding box
+var minMapWidthIteration = 0
+
+func displayStars(stars []star) {
+	seconds := 0
+	for {
+		seconds++
+		currentWidth := width(stars)
+		if currentWidth < minMapSize {
+			minMapSize = currentWidth
+			minMapWidthIteration = seconds
 		}
-		grid = append(grid, xRow)
+		for i := 0; i < len(stars); i++ {
+			star := stars[i]
+			star.x = star.x + star.vx
+			star.y = star.y + star.vy
+			stars[i] = star
+		}
+
+		// minimum bounding box is achieved in 10244
+		// seconds -1 will print the correct result
+		if seconds == 10243 {
+			printStars(stars)
+		}
+
+		if seconds > maxIteration {
+			fmt.Println("iteration: ", minMapWidthIteration)
+			break
+		}
 	}
-	return grid
 }
 
-func getMaxPowerLevelSquare(grid [][]int, squareSize int) (int, int, int) {
-	maxX := 1
-	maxY := 1
-	maxSquareLevel := squarePowerLevel(1, 1, squareSize, grid)
-	maxSize := len(grid) - squareSize + 1
-	for y := 0; y < maxSize; y++ {
-		for x := 0; x < maxSize; x++ {
-			squareLevel := squarePowerLevel(x, y, squareSize, grid)
-			if squareLevel > maxSquareLevel {
-				maxSquareLevel = squareLevel
-				maxX = x + 1
-				maxY = y + 1
+func printStars(myStars []star) {
+	stars := unique(myStars)
+	// sort stars by y and sort by x ascending in both ways
+	sort.Slice(stars, func(i, j int) bool {
+		if stars[i].y < stars[j].y {
+			return true
+		}
+		if stars[i].y > stars[j].y {
+			return false
+		}
+		return stars[i].x < stars[j].x
+	})
+	fmt.Println(stars)
+
+	iter := 0
+	printed := []star{}
+	for y := -maxHeight / 2; y < maxHeight/2; y++ {
+		for x := -maxWidth / 2; x < maxWidth/2; x++ {
+			if iter >= len(stars) {
+				fmt.Printf(". ")
+				continue
+			}
+
+			star := stars[iter]
+			if star.y == y && star.x == x {
+				printed = append(printed, star)
+				fmt.Printf("x ")
+				iter++
+			} else { // such star does not exist
+				fmt.Printf(". ")
 			}
 		}
+		fmt.Printf("\n")
 	}
-	return maxSquareLevel, maxX, maxY
+	fmt.Println("Len: ", len(stars), "Iter: ", iter, "Printed: ", len(printed))
 }
 
-// 3x3 square power level
-func squarePowerLevel(x, y, squareSize int, grid [][]int) int {
-	squarePowerLevel := 0
-	for dy := 0; dy < squareSize; dy++ {
-		for dx := 0; dx < squareSize; dx++ {
-			newY := y + dy
-			newX := x + dx
-			if newX > len(grid)-1 || newY > len(grid)-1 {
-				return squarePowerLevel
-			}
-			squarePowerLevel += grid[newY][newX]
-		}
-	}
-	return squarePowerLevel
-}
-
-func coordinatePowerLever(x, y, gridID int) int {
-	rackID := x + 10
-	powerLevel := rackID * y
-	powerLevel += gridID
-	powerLevel *= rackID
-	powerLevel = getHundredsOrZero(powerLevel)
-	powerLevel -= 5
-	return powerLevel
-}
-
-func getHundredsOrZero(num int) int {
-	strNum := strconv.Itoa(num)
-	if len(strNum) >= 3 {
-		position := len(strNum) - 1 - 2
-		n, err := strconv.Atoi(string(strNum[position]))
+func convertToNum(numArr []string) []int {
+	ints := make([]int, 0, len(numArr))
+	for _, line := range numArr {
+		num, err := strconv.Atoi(line)
 		if err != nil {
 			log.Fatal(err)
 		}
-		return n
+
+		ints = append(ints, num)
 	}
-	return 0
+	return ints
+}
+
+func parseInput(input []string) []star {
+	findNumbers := regexp.MustCompile(`-?\d+`)
+	stars := []star{}
+	for _, line := range input {
+		parsedNumStrings := findNumbers.FindAllString(line, -1)
+		if len(parsedNumStrings) < 4 {
+			log.Fatal("Less than 4 parsed numbers")
+		}
+		n := convertToNum(parsedNumStrings)
+		star := star{x: n[0], y: n[1], vx: n[2], vy: n[3]}
+		stars = append(stars, star)
+	}
+	fmt.Println(stars[len(stars)-1])
+	return stars
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func max(stars []star) (int, int) {
+	maxX := stars[0].x
+	maxY := stars[0].y
+
+	for _, star := range stars {
+		if star.x > maxX {
+			maxX = star.x
+		}
+		if star.y > maxY {
+			maxY = star.y
+		}
+	}
+	return maxX, maxY
+}
+
+func min(stars []star) (int, int) {
+	minX := stars[0].x
+	minY := stars[0].y
+
+	for _, star := range stars {
+		if star.x < minX {
+			minX = star.x
+		}
+
+		if star.y < minY {
+			minY = star.y
+		}
+	}
+	return minX, minY
+}
+
+func width(stars []star) int {
+	minX, minY := min(stars)
+	maxX, maxY := max(stars)
+	return abs(minX-maxX) + abs(maxY-minY)
+}
+
+func unique(intSlice []star) []star {
+	keys := make(map[star]bool)
+	list := []star{}
+	for _, entry := range intSlice {
+		// reducing coordinates to move them more towards the center of the screen (instead of right down part)
+		entry.x -= 170
+		entry.y -= 170
+		entry.vx = 0
+		entry.vy = 0
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
 }
--- 2018/11/main.go
+++ 2018/11/main.go
@@ -1,115 +1,103 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
-	"strings"
+	"strconv"
 )
 
+// 11.day: https://adventofcode.com/2018/day/11
 func main() {
-	initialState, growingRules := readInput()
-	initSize := len(initialState)
-	thirdEmpty := make([]string, initSize)
-	for i := 0; i < len(thirdEmpty); i++ {
-		thirdEmpty[i] = "."
-	}
-	indexMovedFor := len(thirdEmpty)
+	const gridID = 6303
+	fmt.Println(coordinatePowerLever(3, 5, 8))
 
-	// we have initial state in the middle
-	allPots := append(thirdEmpty, strings.Split(initialState, "")...)
-	allPots = append(allPots, thirdEmpty...)
+	grid := createGrid(gridID, 300)
+	powerLevel, x, y := getMaxPowerLevelSquare(grid, 3)
+	fmt.Printf("Power level: %d, x: %d, y: %d\n", powerLevel, x, y)
 
-	for gen := 0; gen < 20; gen++ {
-		allPots = createGeneration(allPots, growingRules)
+	maxLevel := 0
+	maxX := 0
+	maxY := 0
+	maxSquareSize := 0
+	// this is brute force approach it could be done better
+	for squareSize := 1; squareSize < 301; squareSize++ {
+		level, x, y := getMaxPowerLevelSquare(grid, squareSize)
+		if level > maxLevel {
+			maxLevel = level
+			maxX = x
+			maxY = y
+			maxSquareSize = squareSize
+			fmt.Println(x, y, maxSquareSize)
+		}
 	}
-	lastGenScore := calculatePotScore(allPots, indexMovedFor)
-	fmt.Println("Part 1, 20th generation pot score:", lastGenScore)
-
-	// part 2
-	hugeEmpty := make([]string, initSize*10)
-	for i := 0; i < len(hugeEmpty); i++ {
-		hugeEmpty[i] = "."
-	}
-	indexMovedFor = len(hugeEmpty)
-	part2Pots := append(hugeEmpty, strings.Split(initialState, "")...)
-	part2Pots = append(part2Pots, hugeEmpty...)
-	previous := 0
-	difference := 0
-	// 200 is magic number to get the constant difference for each generation iteration
-	const reachConstantIncrease = 200
-	for gen := 0; gen < reachConstantIncrease; gen++ {
-		part2Pots = createGeneration(part2Pots, growingRules)
-		score := calculatePotScore(part2Pots, indexMovedFor)
-		difference = score - previous
-		previous = score
-	}
-	currentScore := calculatePotScore(part2Pots, indexMovedFor)
-	const endGeneration = 50000000000
-	fmt.Printf("Part2 pot score after %d years: %d\n", endGeneration, currentScore+(endGeneration-reachConstantIncrease)*difference)
-
+	fmt.Printf("Max level of all squares: x,y,squareSize %d,%d,%d\n", maxX, maxY, maxSquareSize)
 }
 
-func createGeneration(allPots []string, growingRules [][]string) []string {
-	// 2 is magic number we can't apply any rules to left/right most elements
-	nextGeneration := append([]string{}, allPots...)
-	for pot := 2; pot < len(allPots)-2; pot++ {
-		for r := 0; r < len(growingRules); r++ {
-			rule := growingRules[r]
-			r1, r2, middle, r3, r4 := rule[0], rule[1], rule[2], rule[3], rule[4]
-			p1, p2, middlePot, p3, p4 := allPots[pot-2], allPots[pot-1], allPots[pot], allPots[pot+1], allPots[pot+2]
-			// check if middle pot matches with middle rule element
-			if middlePot == middle {
-				// we have a match, next generation this position will have a pot
-				if r1 == p1 && r2 == p2 && r3 == p3 && r4 == p4 {
-					nextGeneration[pot] = "#"
-					break
-				}
-			}
-			// check last element did not break (change an element to the pot)
-			if r == len(growingRules)-1 {
-				nextGeneration[pot] = "."
+func createGrid(gridID int, gridSize int) [][]int {
+	grid := [][]int{}
+	for y := 0; y < gridSize; y++ {
+		xRow := make([]int, gridSize)
+		for x := 0; x < gridSize; x++ {
+			xRow[x] = coordinatePowerLever(x+1, y+1, gridID)
+		}
+		grid = append(grid, xRow)
+	}
+	return grid
+}
+
+func getMaxPowerLevelSquare(grid [][]int, squareSize int) (int, int, int) {
+	maxX := 1
+	maxY := 1
+	maxSquareLevel := squarePowerLevel(1, 1, squareSize, grid)
+	maxSize := len(grid) - squareSize + 1
+	for y := 0; y < maxSize; y++ {
+		for x := 0; x < maxSize; x++ {
+			squareLevel := squarePowerLevel(x, y, squareSize, grid)
+			if squareLevel > maxSquareLevel {
+				maxSquareLevel = squareLevel
+				maxX = x + 1
+				maxY = y + 1
 			}
 		}
 	}
-	return nextGeneration
+	return maxSquareLevel, maxX, maxY
 }
 
-func calculatePotScore(pots []string, indexMovedFor int) int {
-	score := 0
-	for i := 0; i < len(pots); i++ {
-		if pots[i] == "#" {
-			score += i - indexMovedFor
-		}
-	}
-	return score
-}
-
-func readInput() (string, [][]string) {
-	file, err := os.Open("input.txt")
-	// file, err := os.Open("testInput.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(file)
-	line := 0
-	initialState := ""
-	rules := [][]string{}
-	for scanner.Scan() {
-		if line == 0 {
-			initialState = strings.Trim(strings.Split(scanner.Text(), ":")[1], " ")
-		} else if line == 1 {
-			line++
-			continue
-		} else {
-			rule := scanner.Text()
-			if strings.Contains(rule, "=> #") {
-				row := strings.Split(strings.Split(rule, " ")[0], "")
-				rules = append(rules, row)
+// 3x3 square power level
+func squarePowerLevel(x, y, squareSize int, grid [][]int) int {
+	squarePowerLevel := 0
+	for dy := 0; dy < squareSize; dy++ {
+		for dx := 0; dx < squareSize; dx++ {
+			newY := y + dy
+			newX := x + dx
+			if newX > len(grid)-1 || newY > len(grid)-1 {
+				return squarePowerLevel
 			}
+			squarePowerLevel += grid[newY][newX]
 		}
-		line++
 	}
-	return initialState, rules
+	return squarePowerLevel
+}
+
+func coordinatePowerLever(x, y, gridID int) int {
+	rackID := x + 10
+	powerLevel := rackID * y
+	powerLevel += gridID
+	powerLevel *= rackID
+	powerLevel = getHundredsOrZero(powerLevel)
+	powerLevel -= 5
+	return powerLevel
+}
+
+func getHundredsOrZero(num int) int {
+	strNum := strconv.Itoa(num)
+	if len(strNum) >= 3 {
+		position := len(strNum) - 1 - 2
+		n, err := strconv.Atoi(string(strNum[position]))
+		if err != nil {
+			log.Fatal(err)
+		}
+		return n
+	}
+	return 0
 }
